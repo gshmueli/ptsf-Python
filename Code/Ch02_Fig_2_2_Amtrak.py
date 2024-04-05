d06c6c10a48fc763bdd2bbe4bb1edc66a20fbeb7
ericjb <ericjberger@gmail.com>
2024-04-05 22:59:13 +0300

    Fig 2-2 and Sept11Travel.csv added

---
+++ Code/Ch02_Fig_2_2_Amtrak.py
@@ -0,0 +1,17 @@
+""" Code to create Figure 2-2 """
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+from sktime.utils import plot_series
+from ptsf_setup import ptsf_theme
+
+style.use('ggplot') ## ggplot theme for plots
+fig, ax = plt.subplots(figsize=(5, 3)) ## aspect ratio 5 x 3
+
+ridership = pd.read_csv('ptsf-Python/Data/Amtrak data.csv', parse_dates=['Month'], index_col='Month')
+ax = plot_series(ridership, ax=ax)
+
+ptsf_theme(ax, colors=['black'], idx=[0]) ## override some sktime.plot_series() defaults
+plt.savefig('Ch02_Fig_2_2_Amtrak.pdf', format='pdf', bbox_inches='tight')
+plt.show()
