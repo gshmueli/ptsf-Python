98d24b613b98b869578e80d3ebb241d4e4788bff
ericjb <ericjberger@gmail.com>
2024-10-23 09:29:47 +0300

    modify Ch03_Fig_3_3_Amtrak.py

--- Code/Ch03_Fig_3_3_Amtrak.py
+++ Code/Ch03_Fig_3_3_Amtrak.py
@@ -4,7 +4,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.style as style
 from sktime.utils import plot_series
-from sktime.forecasting.base import ForecastingHorizon
 from sktime.forecasting.model_selection import temporal_train_test_split
 from sktime.forecasting.trend import PolynomialTrendForecaster
 from ptsf_setup import ptsf_theme
@@ -18,10 +17,10 @@ ridership_train, ridership_test = temporal_train_test_split(ridership, test_size
 # Fit model to training period
 forecaster = PolynomialTrendForecaster(degree=2)
 forecaster.fit(ridership_train)
-fitted_values = forecaster.predict(ForecastingHorizon(ridership_train.index, is_relative=False))
+fitted_values = forecaster.predict(ridership_train.index)
 
 # Get forecasts for test period
-pred_values = forecaster.predict(ForecastingHorizon(ridership_test.index, is_relative=False))
+pred_values = forecaster.predict(ridership_test.index)
 
 style.use('ggplot')
 figure, axs = plt.subplots(2, 1, sharex=False, figsize=(6, 4))
@@ -36,7 +35,7 @@ axs[0] = ptsf_train_test(axs[0], ridership_train.index, ridership_test.index)
 axs[1] = plot_series(ridership_train - fitted_values, ridership_test - pred_values, ax=axs[1],
                         title='Errors', y_label='Error')
 ptsf_theme(axs[1], colors=['black']*2, idx=[0,1], lty=['-', '--'])
-axs[1].axhline(0, color='.4', linestyle='-') 
+axs[1].axhline(0, color='.4', linestyle='-')
 axs[1].axvline(x=ridership_train.index[-1], color='0.37', linewidth=1.3)
 plt.subplots_adjust(hspace=0.5)
 
