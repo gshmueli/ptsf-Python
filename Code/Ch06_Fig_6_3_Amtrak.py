19c82e708e3ec52e193537920819480fa6a3d18b
ericjb <ericjberger@gmail.com>
2024-10-24 17:00:05 +0300

    modified Ch06_Fig_6_3_Amtrak.py

--- Code/Ch06_Fig_6_3_Amtrak.py
+++ Code/Ch06_Fig_6_3_Amtrak.py
@@ -5,28 +5,28 @@ import matplotlib.pyplot as plt
 import matplotlib.style as style
 from sktime.utils import plot_series
 from sktime.forecasting.model_selection import temporal_train_test_split
-from sktime.forecasting.trend import PolynomialTrendForecaster
+from sktime.forecasting.ardl import ARDL
 from ptsf_setup import ptsf_theme
 from ptsf_setup import ptsf_train_test
 
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 ridership = pd.read_csv('ptsf-Python/Data/Amtrak.csv', parse_dates=['Month'], index_col='Month')
-ridership.index = ridership.index.to_period('M').to_timestamp()
+ridership.index = ridership.index.to_period('M')
 test_size = len(ridership.truncate(before='2001-04-01'))
-train, test = temporal_train_test_split(ridership, test_size=test_size)
+ridership_train, ridership_test = temporal_train_test_split(ridership, test_size=test_size)
 
 style.use('ggplot')
 
-lm = PolynomialTrendForecaster(degree=1)
-lm.fit(train)
-fitted_values = lm.predict(train.index)
+lm = ARDL(lags=0, trend='ct', seasonal=False, auto_ardl=False)
+lm.fit(ridership_train, X=None)
+fitted_values = lm.predict(ridership_train.index)
+pred = lm.predict(ridership_test.index)
 
-pred = lm.predict(test.index)
 fig, ax = plt.subplots(figsize=(6,4))
 ax = plot_series(ridership, fitted_values, pred, markers=['']*3,
             labels=None, x_label="", y_label="Ridership", ax=ax)
 ptsf_theme(ax, colors=['black','blue','blue'], idx=[0,1,2], lty=['-','-','--'])
-ax = ptsf_train_test(ax, train.index, test.index)
+ax = ptsf_train_test(ax, ridership_train.index, ridership_test.index)
 plt.savefig('Ch06_Fig_6_3_Amtrak.pdf', format='pdf', bbox_inches='tight')
 plt.show()
