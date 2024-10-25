b235f47f3f56b80b34d7050c48bdf1e5e47c735e
ericjb <ericjberger@gmail.com>
2024-10-25 21:40:14 +0300

    added Ch06_Table_6_3_Amtrak.py

---
+++ Code/Ch06_Table_6_3_Amtrak.py
@@ -0,0 +1,15 @@
+""" Code to create Table 6.3 """
+import warnings
+import pandas as pd
+from sktime.forecasting.model_selection import temporal_train_test_split
+from sktime.forecasting.ardl import ARDL
+
+warnings.filterwarnings('ignore', category=FutureWarning)
+
+ridership = pd.read_csv('ptsf-Python/Data/Amtrak.csv', parse_dates=['Month'], index_col='Month')
+ridership.index = ridership.index.to_period('M').to_timestamp()
+test_size = len(ridership.truncate(before='2001-04-01'))
+train, test = temporal_train_test_split(ridership, test_size=test_size)
+ardl_seas = ARDL(lags=0, order=0, trend='c', seasonal=True, auto_ardl=False)
+ardl_seas.fit(train, X=None)
+print(ardl_seas.summary())
