94e6420c0fb6a49f0275712a45e5128d933214ef
ericjb <ericjberger@gmail.com>
2025-01-09 22:55:12 +0200

    renamed variable n_valid to n_test

--- Code/Ch03_Table_3_3_Amtrak.py
+++ Code/Ch03_Table_3_3_Amtrak.py
@@ -16,13 +16,13 @@ ridership.index = pd.to_datetime(ridership.index, format='%Y %b')
 
 y = ridership.copy()
 y.index = y.index.to_period('M').to_timestamp('M')
-n_valid = 36
-y_train, y_valid = temporal_train_test_split(y, test_size=n_valid)
+n_test = 36
+y_train, y_valid = temporal_train_test_split(y, test_size=n_test)
 forecaster = NaiveForecaster(strategy='last')
 
 ## Roll-forward calculations
 mae, rmse, mape = (lambda: ([], [], []))()
-for fh in np.arange(1,n_valid + 1):
+for fh in np.arange(1,n_test + 1):
     cv = ExpandingWindowSplitter(initial_window=len(y_train), step_length=1, fh=[fh])
     res = evaluate(forecaster=forecaster, y=y, cv=cv, return_data=True, error_score='raise',
                         scoring=[MeanAbsoluteError(), mean_absolute_percentage_error])
@@ -35,13 +35,13 @@ mean_values = list(map(lambda x: np.mean(np.array(x)), [mae, rmse, mape]))
 
 ## Fixed partition calculations
 forecaster.fit(y_train)
-y_pred = forecaster.predict(fh=np.arange(n_valid)+1)
+y_pred = forecaster.predict(fh=np.arange(n_test)+1)
 mae_b = mean_absolute_error(y_valid, y_pred)
 rmse_b = np.sqrt(mean_squared_error(y_valid, y_pred))
 mape_b = mean_absolute_percentage_error(y_valid, y_pred)
 fixed = [mae_b, rmse_b, mape_b]
 
-a = ["Roll-forward " + str(i) + "-month-ahead" for i in range(1,n_valid+1)]
+a = ["Roll-forward " + str(i) + "-month-ahead" for i in range(1,n_test+1)]
 df = pd.DataFrame({'Method': a, 'MAE': mae, 'RMSE': rmse, 'MAPE': mape})
 mean_row = pd.DataFrame([['Roll-forward overall'] + mean_values], columns=df.columns)
 fixed_row = pd.DataFrame([['Fixed partitioning overall'] + fixed], columns=df.columns)
