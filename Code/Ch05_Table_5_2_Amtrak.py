c1928b03ad2ba57d5361a8af5eb8edd33a74ad7a
ericjb <ericjberger@gmail.com>
2025-01-11 19:14:20 +0200

    modified Ch05_Table_5_2_Amtrak.py

--- Code/Ch05_Table_5_2_Amtrak.py
+++ Code/Ch05_Table_5_2_Amtrak.py
@@ -13,9 +13,7 @@ y.index = y.index.to_period('M')
 n_test = 36
 y_train, y_test = temporal_train_test_split(y, test_size=n_test)
 
-hwin = AutoETS(auto=False, error="mul", trend="add", seasonal="add", sp=12, 
-               start_params = np.array([ 0.2, 0.2, 0.2, 1838, 0.5, 28.44, -11.22, -0.53, 
-                    -124.28, 200.20, 146.81, 36.59, 76.04, 60.15, 44.18, -249.47, -206.92]), n_jobs=-1)
+hwin = AutoETS(auto=False, error="mul", trend="add", seasonal="add", sp=12, n_jobs=-1)
 hwin.fit(y_train)
 print(hwin._fitted_forecaster.summary())
 print("\n\nFinal states:\n=============")
