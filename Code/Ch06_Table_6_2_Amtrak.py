29da53ae2950529cf0356be92e228428bcbfe94a
ericjb <ericjberger@gmail.com>
2025-01-24 22:22:14 +0200

    replace ARDL model by PolynomialTrendForecaster; use ptsf_summary

--- Code/Ch06_Table_6_2_Amtrak.py
+++ Code/Ch06_Table_6_2_Amtrak.py
@@ -3,20 +3,17 @@ import warnings
 import pandas as pd
 import numpy as np
 from sktime.forecasting.model_selection import temporal_train_test_split
-from sktime.forecasting.ardl import ARDL
+from sktime.forecasting.trend import PolynomialTrendForecaster
+from ptsf_setup import ptsf_summary
 
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 ridership = pd.read_csv('ptsf-Python/Data/Amtrak.csv', parse_dates=['Month'], index_col='Month')
-ridership.index = ridership.index.to_period('M').to_timestamp()
+ridership.index = ridership.index.to_period('M')
 test_size = len(ridership.truncate(before='2001-04-01'))
-train, test = temporal_train_test_split(ridership, test_size=test_size)
+ridership_train, ridership_test = temporal_train_test_split(ridership, test_size=test_size)
 
-t = np.arange(1, len(ridership)+1)
-columns = [t**i for i in range(3)]
-X = pd.DataFrame(np.vstack(columns).T, index=ridership.index)
-X.columns = ['const', 't', 't**2']
-ardl_quad = ARDL(lags=0, order=0, trend='n', seasonal=False, auto_ardl=False)
-ardl_quad.fit(train, X=X.iloc[:len(train),:])
-print(ardl_quad.summary())
+qm = PolynomialTrendForecaster(degree=2)  ## quadratic trend model
+qm.fit(ridership_train)
 
+ptsf_summary(qm, ridership_train)
