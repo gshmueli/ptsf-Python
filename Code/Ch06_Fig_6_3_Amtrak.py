6f22c46a928ec7626638c5ba47ce46b45833d2ac
ericjb <ericjberger@gmail.com>
2025-01-24 18:31:57 +0200

    major change use ptsf_summary for Ch06_Fig_6_3_Amtrak.py

--- Code/Ch06_Fig_6_3_Amtrak.py
+++ Code/Ch06_Fig_6_3_Amtrak.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 import matplotlib.style as style
 from sktime.utils import plot_series
 from sktime.forecasting.model_selection import temporal_train_test_split
-from sktime.forecasting.ardl import ARDL
+from sktime.forecasting.trend import TrendForecaster
 from ptsf_setup import ptsf_theme
 from ptsf_setup import ptsf_train_test
+from ptsf_setup import ptsf_summary
 
 warnings.filterwarnings('ignore', category=FutureWarning)
 
@@ -18,8 +19,8 @@ ridership_train, ridership_test = temporal_train_test_split(ridership, test_size
 
 style.use('ggplot')
 
-lm = ARDL(lags=0, trend='ct', seasonal=False, auto_ardl=False)
-lm.fit(ridership_train, X=None)
+lm = TrendForecaster()
+lm.fit(ridership_train)
 fitted = lm.predict(ridership_train.index)
 pred = lm.predict(ridership_test.index)
 
@@ -30,3 +31,5 @@ ptsf_theme(ax, colors=['black','blue','blue'], idx=[0,1,2], lty=['-','-','--'])
 ax = ptsf_train_test(ax, ridership_train.index, ridership_test.index)
 plt.savefig('Ch06_Fig_6_3_Amtrak.pdf', format='pdf', bbox_inches='tight')
 plt.show()
+
+ptsf_summary(lm, ridership_train)
