82dc5ca0603af28d44ac6d3a9ef378c7dcd3571b
ericjb <ericjberger@gmail.com>
2025-02-06 22:37:14 +0200

    rewrite of Ch07_Fig_7_7_Bike.py

--- Code/Ch07_Fig_7_7_Bike.py
+++ Code/Ch07_Fig_7_7_Bike.py
@@ -1,14 +1,15 @@
-""" Code to create Figure 7.7 """
+""" Code to create Figure 7.7 and Table 7.2"""
 import warnings
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.style as style
-from sktime.forecasting.ardl import ARDL
 from sktime.forecasting.model_selection import temporal_train_test_split
 from sktime.utils import plot_series
 from sktime.performance_metrics.forecasting import mean_absolute_error, mean_squared_error, \
         mean_absolute_percentage_error, mean_absolute_scaled_error
+import statsmodels.api as sm
+from statsmodels.tools import add_constant
 from ptsf_setup import ptsf_theme
 from ptsf_setup import ptsf_train_test
 
@@ -29,75 +30,51 @@ warnings.filterwarnings('ignore', category=FutureWarning)
 bike = pd.read_csv('ptsf-Python/Data/BikeSharingDaily.csv',parse_dates=True,index_col=1)
 bike.index = pd.DatetimeIndex(bike.index).to_period('D')
 
-# Create X - the exogenous variables
-months = pd.get_dummies(bike.index.strftime('%b')).set_index(bike.index).drop(['Jan'], axis=1)
-dow = pd.get_dummies(bike.index.strftime('%a')).set_index(bike.index).drop(['Thu'], axis=1)
-working = pd.get_dummies(bike['workingday'].astype('category'), prefix='Z')
-working = working.rename(columns = {'Z_0': 'Nonworking', 'Z_1': 'Working'})
-weather = pd.get_dummies(bike['weathersit'].astype('category'), prefix='Z')
-weather = weather.rename(columns = {'Z_1': 'Clear', 'Z_2': 'Mist', 'Z_3': 'RainSnow'})
-inter_cols = [f'{col1}_{col2}' \
-    for i, col1 in enumerate(working.columns) for j, col2 in enumerate(weather.columns)]
-tmp_df = pd.concat([working, weather], axis=1)
-inter = tmp_df.assign(**{col: tmp_df.eval(col.replace('_','*')) for col in inter_cols})
-X = pd.concat([months[['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov','Dec']],
-    inter[['Working_Clear','Nonworking_Mist','Working_Mist','Nonworking_RainSnow','Working_RainSnow']],
-    dow[['Fri', 'Sat', 'Sun','Mon','Tue','Wed']]], axis=1)
+months_order = bike.index.strftime('%b').unique()
+months = pd.get_dummies(pd.Categorical(bike.index.strftime('%b'), 
+                        categories=months_order, ordered=True), dtype=int).drop(['Jan'], axis=1)
+dow = pd.get_dummies(bike.index.strftime('%a'), dtype=int).drop(['Thu'], axis=1)
+a = pd.get_dummies(bike['workingday'].astype('category'), prefix='Z', dtype=int)
+a.columns = ['Nonworking' if col == 'Z_0' else 'Working' for col in a.columns]
+b = pd.get_dummies(bike['weathersit'].astype('category'), prefix='Z', dtype=int)
+b.columns = ['Clear' if col == 'Z_1' else 'Mist' if col == 'Z_2' else 'RainSnow' for col in b.columns]
+c = pd.concat([a, b], axis=1).assign(**{f'{v}_{w}': a[v] * b[w] for v in a.columns for w in b.columns})
+months, dow, c = map(lambda df: df.set_index(bike.index), [months, dow, c]) # ensure same index
+X = pd.concat([months, c.iloc[:, -5:], dow], axis=1)
+X['trend'] = np.arange(1, len(X) + 1)
+X = add_constant(X)   # to get the intercept
 
-y_train, y_test = temporal_train_test_split(bike['cnt'], test_size=90)
-n_train = len(y_train)
+train, test = temporal_train_test_split(bike['cnt'], test_size=90)
 
-mdl = ARDL(lags=0, order=0, trend='ct', seasonal=False, auto_ardl=False)
-_ = mdl.fit(y_train, X=X.iloc[:n_train,:])
+mdl = sm.OLS(train.values, X[:len(train)]).fit()
 
-fitted = mdl.predict(y_train.index, X=X.iloc[:n_train,:])
-pred = mdl.predict(y_test.index, X=X.iloc[n_train:,:])
-
-resid_train = y_train - fitted
-resid_test = y_test - pred
+fitted = mdl.predict(X.iloc[:len(train),:])
+pred = mdl.predict(X.iloc[len(train):,:])
 
 figure, axis = plt.subplots(nrows=2, sharex=False, figsize=(12, 8))
+figure.subplots_adjust(hspace=0.3)
 
+# Extract lower and upper confidence intervals
+alpha=0.05
+pi = mdl.get_prediction(X.iloc[len(train):,:]).summary_frame(alpha=alpha) #95% confidence interval
+
+pred_interval = pd.DataFrame({'lower': pi['obs_ci_lower'], 'upper': pi['obs_ci_upper']}, index=test.index)
+pred_interval.columns = pd.MultiIndex.from_product([['cnt'],[1-alpha], ['lower', 'upper']])
 axis[0] = plot_series(bike['cnt'], fitted, pred, markers=['']*3, ax=axis[0],
-            title="Rentals and Forecasts", labels=None, y_label="Count")
+            pred_interval=pred_interval,  title="Rentals and Forecasts", labels=None, y_label="Count")
 ptsf_theme(axis[0], colors=['black','blue','blue'], idx=[0,1,2], lty=['-','-','--'])
-axis[0] = ptsf_train_test(axis[0], y_train.index, y_test.index)
+axis[0] = ptsf_train_test(axis[0], train.index, test.index)
 
-axis[1] = plot_series(resid_train, resid_test, markers=['']*2, ax=axis[1],
+
+axis[1] = plot_series(train - fitted, test - pred, markers=['']*2, ax=axis[1],
             title="Errors", labels=None, y_label="Error")
 ptsf_theme(axis[1], colors=['black']*2, idx=[0,1], lty=['-','--'])
+
 plt.savefig('Ch07_Fig_7_7_Bike.pdf', format='pdf', bbox_inches='tight')
 plt.show()
 
 print(mdl.summary())
 
 print(pd.concat([
-    accuracy_df("Train", y_train, fitted, y_train),
-    accuracy_df("Test", y_test, pred, y_train)]).drop('MASE',axis=1).round(0))
-
-""" # The following code produces the plot with the prediction intervals
-import statsmodels.api as sm
-import numpy as np
-Z = bike['cnt'].to_frame()
-Z = Z.merge(X, left_index=True, right_index=True, how='left')
-Z['trend'] = np.arange(1, len(X)+1)
-form = 'cnt ~ ' + ' + '.join(list(Z.columns)[1:])
-Z_train = Z.iloc[:n_train,:]
-Z_test = Z.iloc[n_train:,:]
-bike_lm = sm.OLS.from_formula(form, data=Z_train)
-bike_fit = bike_lm.fit()
-bike_pred = bike_fit.get_prediction(exog=Z_test.drop(['cnt'], axis=1))
-alpha=0.05
-prediction_intervals = bike_pred.summary_frame(alpha=alpha) #95% confidence interval
-# Extract lower and upper confidence intervals
-lower = prediction_intervals['obs_ci_lower']
-upper = prediction_intervals['obs_ci_upper']
-pred_interval = pd.DataFrame({'lower': lower, 'upper': upper})
-pred_interval.index=pred.index
-pred_interval.columns = pd.MultiIndex.from_product([['cnt'],[1-alpha], ['lower', 'upper']])
-fig, ax = plot_series(bike['cnt'], fitted, pred, markers=['']*3, pred_interval=pred_interval,
-            title="Rentals and Forecasts", labels=None, y_label="Count")
-ptsf_theme(ax, colors=['black','blue','blue'], idx=[0,1,2], lty=['-','-','--'])
-plt.show()
- """
- 
+    accuracy_df("Train", train, fitted, train),
+    accuracy_df("Test", test, pred, train)]).drop('MASE',axis=1).round(0))
