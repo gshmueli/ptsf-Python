ac7832ae5c28bd407bd822db4e96dcc24b3b703f
ericjb <ericjberger@gmail.com>
2025-02-08 19:09:29 +0200

    yet more modifications to Ch09_Fig_9_1_MelbourneRainfall.py

--- Code/Ch09_Fig_9_1_MelbourneRainfall.py
+++ Code/Ch09_Fig_9_1_MelbourneRainfall.py
@@ -8,23 +8,23 @@ import seaborn as sns
 style.use('ggplot')
 
 # Read the data
-rainfall = pd.read_csv('ptsf-Python/Data/MelbourneRainfall.csv', parse_dates=['Date'], index_col='Date')
-rainfall.index = pd.to_datetime(rainfall.index, format='%d/%m/%Y')
+rain = pd.read_csv('ptsf-Python/Data/MelbourneRainfall.csv', parse_dates=['Date'], index_col='Date')
+rain.index = pd.to_datetime(rain.index, format='%d/%m/%Y')
 
 # Create additional columns: 'rainy', 'Year', 'Month'
-rainfall['rainy'] = np.where(rainfall['RainfallAmount_millimetres'] > 0, 1, 0)
-rainfall['Year'] = rainfall.index.year
-rainfall['Month'] = rainfall.index.month
+rain['rainy'] = np.where(rain['RainfallAmount_millimetres'] > 0, 1, 0)
+rain['Year'] = rain.index.year
+rain['Month'] = rain.index.month
 
 # Calculate monthly and "month-year" rain percentages
-monthly_yearly_rain = pd.DataFrame({'pct': rainfall.groupby(['Year', 'Month'])['rainy'].mean() * 100}).reset_index()
-monthly_rain = pd.DataFrame({'pct': rainfall.groupby('Month')['rainy'].mean() * 100}).reset_index()
+month_year_rain = pd.DataFrame({'pct': rain.groupby(['Year', 'Month'])['rainy'].mean() * 100}).reset_index()
+month_rain = pd.DataFrame({'pct': rain.groupby('Month')['rainy'].mean() * 100}).reset_index()
 
 plt.figure(figsize=(12, 6))
-sns.lineplot(data=monthly_yearly_rain, x='Month', y='pct', hue='Year', marker='', legend='full')
+sns.lineplot(data=month_year_rain, x='Month', y='pct', hue='Year', marker='', legend='full')
 
-# Overlay the monthly average rainfall
-sns.lineplot(data=monthly_rain, x='Month', y='pct', color='black', linestyle='--', linewidth=2, label='Average')
+# Overlay the monthly average rain
+sns.lineplot(data=month_rain, x='Month', y='pct', color='black', linestyle='--', linewidth=2, label='Average')
 
 # Add labels and legend
 plt.xlabel('')
