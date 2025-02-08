b526bb521b05b43f27760b606c9b81e04c6310a0
ericjb <ericjberger@gmail.com>
2025-02-08 21:02:45 +0200

    minor change to Ch09_Table_9_2_MelbourneRainfall.py

--- Code/Ch09_Table_9_2_MelbourneRainfall.py
+++ Code/Ch09_Table_9_2_MelbourneRainfall.py
@@ -11,7 +11,8 @@ rainfall = rainfall.assign(t = np.arange(1,len(rainfall)+1,1),
     Seasonal_cosine = lambda x: np.cos(2*np.pi*x['t']/365.25)
     )
 rainfall['rainy'] = np.where(rainfall['RainfallAmount_millimetres'] > 0, 1, 0)
-rainfall = rainfall.assign(Lag1=rainfall['rainy'].shift(1)).iloc[1:,:] ## drop first row that has NA in lag
+rainfall = rainfall.assign(Lag1=rainfall['rainy'].shift(1))
+rainfall = rainfall.iloc[1:,:] ## drop first row that has NA in lag
 
 rain_train = rainfall.truncate(after='2009-12-31')
 rain_test = rainfall.truncate(before='2010-01-01')
@@ -22,7 +23,7 @@ exog_test = rain_test[['Lag1','Seasonal_sine','Seasonal_cosine']]
 lr_model = LogisticRegression(random_state=0, fit_intercept=True, penalty=None)
 lr_model.fit(exog_train, rain_train['rainy'])
 
-print(pd.DataFrame({'Coef': ['Intercept'] + exog_train.columns.tolist(), 
+print(pd.DataFrame({'Coef': ['Intercept'] + exog_train.columns.tolist(),
               'Estimate':np.append(lr_model.intercept_, lr_model.coef_[0])}))
 
 fitted_probs = lr_model.predict_proba(exog_train)
