a3c52fae506808db40e6e511a2c41693b5ac119c
ericjb <ericjberger@gmail.com>
2025-02-08 21:10:41 +0200

    replaced glm analysis in Table 9.3 with probs from logistic model

--- Code/Ch09_Table_9_3_MelbourneRainfall.py
+++ Code/Ch09_Table_9_3_MelbourneRainfall.py
@@ -1,7 +1,7 @@
 """ Code to create Table 9.3 """
 import pandas as pd
 import numpy as np
-import statsmodels.api as sm
+from sklearn.linear_model import LogisticRegression
 
 rainfall = pd.read_csv('ptsf-Python/Data/MelbourneRainfall.csv', parse_dates=['Date'], index_col='Date')
 rainfall.index = pd.to_datetime(rainfall.index, format='%d/%m/%Y')
@@ -11,7 +11,8 @@ rainfall = rainfall.assign(t = np.arange(1,len(rainfall)+1,1),
     Seasonal_cosine = lambda x: np.cos(2*np.pi*x['t']/365.25)
     )
 rainfall['rainy'] = np.where(rainfall['RainfallAmount_millimetres'] > 0, 1, 0)
-rainfall = rainfall.assign(Lag1=rainfall['rainy'].shift(1)).iloc[1:,:] ## drop first row that has NA in lag
+rainfall = rainfall.assign(Lag1=rainfall['rainy'].shift(1))
+rainfall = rainfall.iloc[1:,:] ## drop first row that has NA in lag
 
 rain_train = rainfall.truncate(after='2009-12-31')
 rain_test = rainfall.truncate(before='2010-01-01')
@@ -19,13 +20,11 @@ rain_test = rainfall.truncate(before='2010-01-01')
 exog_train = rain_train[['Lag1','Seasonal_sine','Seasonal_cosine']]
 exog_test = rain_test[['Lag1','Seasonal_sine','Seasonal_cosine']]
 
-# Add a constant term for the intercept
-exog_train = sm.add_constant(exog_train)
-exog_test = sm.add_constant(exog_test)
+lr_model = LogisticRegression(random_state=0, fit_intercept=True, penalty=None)
+lr_model.fit(exog_train, rain_train['rainy'])
 
-# Fit the logistic regression model using GLM with MLE
-glm_model = sm.GLM(rain_train['rainy'], exog_train, family=sm.families.Binomial())
-glm_results = glm_model.fit()
+fitted_probs = lr_model.predict_proba(exog_train)
+print(pd.DataFrame(fitted_probs, columns=['Prob no rain', 'Prob rain'], index=exog_train.index))
 
-# Print the summary of the model
-print(glm_results.summary())
+pred_probs = lr_model.predict_proba(exog_test)
+print(pd.DataFrame(pred_probs, columns=['Prob no rain', 'Prob rain'], index=exog_test.index))
