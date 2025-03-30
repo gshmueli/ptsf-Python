05d3f26866a872c36dd9655cae0f855567bd7a6f
ericjb <ericjberger@gmail.com>
2025-03-30 19:37:01 +0300

    modified Ch06_Table_6_2_Amtrak.py to use ARDL

--- Code/Ch06_Table_6_2_Amtrak.py
+++ Code/Ch06_Table_6_2_Amtrak.py
@@ -3,17 +3,15 @@ import warnings
 import pandas as pd
 import numpy as np
 from sktime.forecasting.model_selection import temporal_train_test_split
-from sktime.forecasting.trend import PolynomialTrendForecaster
-from ptsf_setup import ptsf_summary
+from sktime.forecasting.ardl import ARDL
 
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 ridership = pd.read_csv('ptsf-Python/Data/Amtrak.csv', parse_dates=['Month'], index_col='Month')
 ridership.index = ridership.index.to_period('M')
 test_size = len(ridership.truncate(before='2001-04-01'))
-ridership_train, ridership_test = temporal_train_test_split(ridership, test_size=test_size)
+train, test = temporal_train_test_split(ridership, test_size=test_size)
 
-qm = PolynomialTrendForecaster(degree=2)  ## quadratic trend model
-qm.fit(ridership_train)
-
-ptsf_summary(qm, ridership_train)
+qm = ARDL(lags=0, trend='ctt', seasonal=False, auto_ardl=False)
+qm.fit(train)
+print(qm.summary())
