3ff558bfd90266d041c081784baec7900fe486f6
ericjb <ericjberger@gmail.com>
2025-03-31 09:32:45 +0300

    modified Ch06_Table_6_3_Amtrak.py to use ARDL

--- Code/Ch06_Table_6_3_Amtrak.py
+++ Code/Ch06_Table_6_3_Amtrak.py
@@ -2,19 +2,14 @@
 import warnings
 import pandas as pd
 from sktime.forecasting.model_selection import temporal_train_test_split
-import statsmodels.api as sm
+from sktime.forecasting.ardl import ARDL
 
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 ridership = pd.read_csv('ptsf-Python/Data/Amtrak.csv', parse_dates=['Month'], index_col='Month')
-ridership.index = ridership.index.to_period('M')
-
-# Create dummy variables for the months and drop January (Month_1)
-mat = pd.get_dummies(ridership.index.month, prefix='Month', dtype=float).drop(columns=['Month_1'])
-X = sm.add_constant(mat)   # add a constant column for the intercept
-
+ridership.index = ridership.index.to_period('M').to_timestamp()
 test_size = len(ridership.truncate(before='2001-04-01'))
 train, test = temporal_train_test_split(ridership, test_size=test_size)
-
-model = sm.OLS(train.values, X[:len(train)]).fit()
-print(model.summary())
+seas = ARDL(lags=0, order=0, trend='c', seasonal=True, auto_ardl=False)
+seas.fit(train, X=None)
+print(seas.summary())
