845ddd19d7fa6ee5b61c303d70865d0cbd3cb817
ericjb <ericjberger@gmail.com>
2025-04-11 10:11:39 +0300

    modified Ch07_Fig_7_4_Amtrak.py to use ARDL

--- Code/Ch07_Fig_7_4_Amtrak.py
+++ Code/Ch07_Fig_7_4_Amtrak.py
@@ -2,39 +2,32 @@
 
 import warnings
 import pandas as pd
-import numpy as np
-from sktime.forecasting.model_selection import temporal_train_test_split
-from sktime.utils import plot_series
-from PyFableARIMA import PyFableARIMA
-import statsmodels.api as sm
 import matplotlib.pyplot as plt
 import matplotlib.style as style
+from sktime.forecasting.model_selection import temporal_train_test_split
+from sktime.utils import plot_series
+from sktime.forecasting.ardl import ARDL
+from PyFableARIMA import PyFableARIMA
 from ptsf_setup import ptsf_theme, ptsf_train_test
 
 warnings.filterwarnings('ignore', category=FutureWarning)
 style.use('ggplot')
 
-ridership = pd.read_csv('ptsf-Python/Data/Amtrak.csv', parse_dates=['Month'], index_col='Month')
+ridership = pd.read_csv('ptsf-Python/Data/Amtrak.csv')
+ridership['Month'] = pd.to_datetime(ridership['Month'], format='%Y %b')
+ridership.set_index('Month', inplace=True)
 ridership.index = ridership.index.to_period('M')
-
-# Create dummy variables for the months and drop January (Month_1)
-mat = pd.get_dummies(ridership.index.month, prefix='Month', dtype=float).drop(columns=['Month_1'])
-
-t = np.arange(1, len(ridership)+1)
-columns = [t**i for i in range(3)]
-X = pd.concat([mat, pd.DataFrame(np.vstack(columns).T)], axis=1)
-X.columns = list(mat.columns) + ['const', 't', 't**2']
 
 test_size = len(ridership.truncate(before='2001-04-01'))
 train, test = temporal_train_test_split(ridership, test_size=test_size)
 
-quad_seas = sm.OLS(train.values, X[:len(train)]).fit()
-fitted = quad_seas.predict(X[:len(train)])
-resid = pd.Series(train.values.flatten() - fitted.values, index=train.index, name='resid')
+quad_seas = ARDL(lags=0, trend='ctt', seasonal=True, auto_ardl=False).fit(train)
+
+fitted = quad_seas.predict(train.index)
+resid = pd.Series((train.squeeze() - fitted.squeeze()), index=train.index, name='resid')
 
 resid_ar = PyFableARIMA(formula='resid ~ 1 + pdq(1,0,0)')
         # '1 +' is used to add a constant to the ARIMA model
-        # for seasonal '+ PDQ(P,D,Q)' (the default is PDQ(0,0,0))
 resid_ar.fit(resid)
 resid_ar_fitted = resid_ar.get_fitted_values()
 
