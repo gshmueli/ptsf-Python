b79abacfd217a4c039935ddd15b0d7e5105652ff
ericjb <ericjberger@gmail.com>
2025-05-24 19:14:41 +0300

    tables were moved around in Chapter 9. Resynch numbering

--- Code/Ch09_Table_9_3_MelbourneRainfall.py
+++ Code/Ch09_Table_9_3_MelbourneRainfall.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
 rainfall = pd.read_csv('ptsf-Python/Data/MelbourneRainfall.csv', parse_dates=['Date'], index_col='Date')
 rainfall.index = pd.to_datetime(rainfall.index, format='%d/%m/%Y')
@@ -11,8 +12,7 @@ rainfall = rainfall.assign(t = np.arange(1,len(rainfall)+1,1),
     Seasonal_cosine = lambda x: np.cos(2*np.pi*x['t']/365.25)
     )
 rainfall['rainy'] = np.where(rainfall['RainfallAmount_millimetres'] > 0, 1, 0)
-rainfall = rainfall.assign(Lag1=rainfall['rainy'].shift(1))
-rainfall = rainfall.iloc[1:,:] ## drop first row that has NA in lag
+rainfall = rainfall.assign(Lag1=rainfall['rainy'].shift(1)).iloc[1:,:] ## drop first row that has NA in lag
 
 rain_train = rainfall.truncate(after='2009-12-31')
 rain_test = rainfall.truncate(before='2010-01-01')
@@ -23,8 +23,14 @@ exog_test = rain_test[['Lag1','Seasonal_sine','Seasonal_cosine']]
 lr_model = LogisticRegression(random_state=0, fit_intercept=True, penalty=None)
 lr_model.fit(exog_train, rain_train['rainy'])
 
-fitted_probs = lr_model.predict_proba(exog_train)
-print(pd.DataFrame(fitted_probs, columns=['Prob no rain', 'Prob rain'], index=exog_train.index))
+def performance_summary(label, pred, actual):
+    tn, fp, fn, tp = confusion_matrix(actual, pred).ravel()
+    print(f"{label} Confusion Matrix\n        Prediction\nActual     0     1")
+    print(f"  0     {tn}   {fp}\n  1     {fn}   {tp}\n\n\nAccuracy : {(tp+tn)/len(actual):.4f}")
+    print(f"Sensitivity : {(tp/(tp+fn)):.4f}\nSpecificity : {(tn/(tn+fp)):.4f}")
 
-pred_probs = lr_model.predict_proba(exog_test)
-print(pd.DataFrame(pred_probs, columns=['Prob no rain', 'Prob rain'], index=exog_test.index))
+train_pred = lr_model.predict(exog_train)
+performance_summary("Training", train_pred, rain_train['rainy'])
+
+test_pred = lr_model.predict(exog_test)
+performance_summary("Test", test_pred, rain_test['rainy'])
--- Code/Ch09_Table_9_4_MelbourneRainfall.py
+++ Code/Ch09_Table_9_4_MelbourneRainfall.py
@@ -1,37 +1,54 @@
 """ Code to create Table 9.4 """
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.ensemble import VotingClassifier
 
+# Load the dataset
 rainfall = pd.read_csv('ptsf-Python/Data/MelbourneRainfall.csv', parse_dates=['Date'], index_col='Date')
 rainfall.index = pd.to_datetime(rainfall.index, format='%d/%m/%Y')
 
-rainfall = rainfall.assign(t = np.arange(1,len(rainfall)+1,1),
-    Seasonal_sine = lambda x: np.sin(2*np.pi*x['t']/365.25),
-    Seasonal_cosine = lambda x: np.cos(2*np.pi*x['t']/365.25)
-    )
+rainfall = rainfall.assign(t=np.arange(1, len(rainfall) + 1, 1),
+                           Seasonal_sine=lambda x: np.sin(2 * np.pi * x['t'] / 365.25),
+                           Seasonal_cosine=lambda x: np.cos(2 * np.pi * x['t'] / 365.25)
+                           )
 rainfall['rainy'] = np.where(rainfall['RainfallAmount_millimetres'] > 0, 1, 0)
-rainfall = rainfall.assign(Lag1=rainfall['rainy'].shift(1)).iloc[1:,:] ## drop first row that has NA in lag
+rainfall = rainfall.assign(Lag1=rainfall['rainy'].shift(1)).iloc[1:, :]  # drop first row that has NA in lag
 
 rain_train = rainfall.truncate(after='2009-12-31')
 rain_test = rainfall.truncate(before='2010-01-01')
 
-exog_train = rain_train[['Lag1','Seasonal_sine','Seasonal_cosine']]
-exog_test = rain_test[['Lag1','Seasonal_sine','Seasonal_cosine']]
-
-lr_model = LogisticRegression(random_state=0, fit_intercept=True, penalty=None)
-lr_model.fit(exog_train, rain_train['rainy'])
+X_train = rain_train[['Lag1', 'Seasonal_sine', 'Seasonal_cosine']]
+X_test = rain_test[['Lag1', 'Seasonal_sine', 'Seasonal_cosine']]
+y_train = rain_train['rainy']
+y_test = rain_test['rainy']
 
 def performance_summary(label, pred, actual):
+    tn, fp, fn, tp = confusion_matrix(actual, pred).ravel()
     print(f"{label} Confusion Matrix")
-    columns = pd.MultiIndex.from_product([['Prediction'], [0, 1]])
-    Z = pd.DataFrame(confusion_matrix(actual, pred), index=[0, 1], columns=columns)
-    Z.index.name = 'Actual'
-    tn, fp, fn, tp = confusion_matrix(actual, pred).ravel()  # tn=true neg, fp=false pos, ...
-    metrics = pd.Series({"Accuracy": (tp + tn) / len(actual),"Sensitivity": tp / (tp + fn),
-                        "Specificity": tn / (tn + fp)})
-    print(f"{Z}\n\n{metrics.to_string(float_format='{:.4f}'.format)}")
+    print("        Prediction")
+    print("Actual     0     1")
+    print(f"  0     {tn}   {fp}")
+    print(f"  1     {fn}   {tp}")
+    print("\n\n")
+    print(f"Accuracy : {(tp+tn)/len(actual):.4f}")
+    print(f"Sensitivity : {(tp/(tp+fn)):.4f}")
+    print(f"Specificity : {(tn/(tn+fp)):.4f}")
 
-performance_summary("Training", lr_model.predict(exog_train), rain_train['rainy'])
-performance_summary("Test", lr_model.predict(exog_test), rain_test['rainy'])
+# Number of MLPClassifiers
+NETWORKS = 20
+HIDDEN_NODES = 2
+
+# Create an ensemble of N MLPClassifier models
+estimators = [(f'mlp{i}', 
+               MLPClassifier(hidden_layer_sizes=(HIDDEN_NODES,), random_state=42+i, max_iter=1000)) 
+              for i in range(NETWORKS)]
+
+ensemble = VotingClassifier(estimators=estimators, voting='soft')
+ensemble.fit(X_train, y_train)
+train_pred = ensemble.predict(X_train)
+test_pred = ensemble.predict(X_test)
+
+performance_summary("Training", train_pred, y_train)
+performance_summary("Test", test_pred, y_test)
